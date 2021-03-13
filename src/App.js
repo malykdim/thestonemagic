6a14092ddfa5446b85aac21923b1050910319d02
Albena Kostova <albena.kostov@gmail.com>
2021-03-13 18:18:44 +0100

    Clear ups

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <h1>The Stone Magic</h1>
+            <h3>
+                Exquisite Mosaics made by<br/>
+                <a className="App-link" href="https://www.facebook.com/vladimir.damynov/" target="_blank"  rel="noopener noreferrer" >
+                    <span>Vladimir Damyanov</span>
+                </a>
+                and
+                <a className="App-link" href="https://www.facebook.com/damyan.damyanov.509/" target="_blank"  rel="noopener noreferrer" >
+                <span>Damyan Damyanov</span>
+                </a>            
+            </h3>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                In Development Stage...
+            </p>        
+        </header>
     </div>
-  );
+    );
 }
 
 export default App;
