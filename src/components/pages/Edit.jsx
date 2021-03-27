cd0b90abcaf13111918d4f69988ef810727fad8c
Albena Kostova <albena.kostov@gmail.com>
2021-03-27 17:25:44 +0100

    Modifying Edit form

--- src/components/pages/Edit.jsx
+++ src/components/pages/Edit.jsx
@@ -10,77 +10,130 @@ class Edit extends Component {
         super(props);
 
         this.state = {
-
-        }
-    }
-
-    render() {
-
-        const onSubmitEditHandler = (e) => {
-            e.preventDefault();
-            
-            const inputPanneauxName = e.target.panneauxName.value;
-            const inputAuthor = e.target.author.value;
-            const inputDate = e.target.created.value;
-            const inputWidth = e.target.width.value;
-            const inputHeight = e.target.height.value;
-            const inputUnit = e.target.unit.value;
-            const inputMaterials = [];
+            panneauxName: 'New Panneaux',
         }
         
-        const logout = (e) => {
-            console.log('Logout');
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
+    
+    onSubmitEditHandler(e) {
+        e.preventDefault();
+        
+        const inputPanneauxName = e.target.panneauxName.value;
+        const inputAuthor = e.target.author.value;
+        const inputDate = e.target.created.value;
+        const inputWidth = e.target.width.value;
+        const inputHeight = e.target.height.value;
+        const inputUnit = e.target.unit.value;
+        // const gridClass = (inputWidth, inputHeight, inputUnit) => {
+        //     if ((inputWidth) === (inputHeight)) {
+        //         const gridClass = 'w-1 h-1';
+        //         return gridClass;
+        //     } else if ((inputWidth) > (inputHeight)) {
+        //         const gridClass = 'w-2 h-1';
+        //         return gridClass;
+        //     } else if ((inputWidth) < (inputHeight)) {
+        //         const gridClass = 'w-1 h-2';
+        //         return gridClass;
+        //     }
+        // }
+        const inputMaterials = [];
+        
+        const data = {
+            url: `/gallery/${inputPanneauxName.toLowerCase()}`,
+            caption: inputPanneauxName,
+            author: inputAuthor,
+            description: {
+                created: inputDate,
+                dimensions: {
+                    width: inputWidth,
+                    height: inputHeight,
+                    unit: inputUnit
+                },
+                materials: [...inputMaterials]
+            },
+            // gridClass: gridClass()
         }
+        
+        const result = data.JSON.stringify;
+        return result;
+    };
+    
+    onSubmitChangeHandler(e) {
+        console.log(e.target.value);
+        this.setState=({
+            panneauxName: e.target.value
+        });
+    };
+    
+    onChangeHandler(e) {
+        this.setState({[e.target.name]: e.target.value});
+    };
+    
+    // logout(e) {
+    //     console.log('Logout');
+    // };
+        
+    render() {
+
+        
+        
+        
         
         return (
             <main className="AppMain">
                 
                 <h2>Edit Page</h2>
                 
-                <form onSubmit={onSubmitEditHandler} className="FormEdit form">
+                <form onSubmit={this.onSubmitEditHandler} className="FormEdit form">
                     
                     
                     <fieldset className="left w-1 h-3">
                         {/* <span>Добави снимка:</span> */}
-                        <label htmlFor="picture">Добави снимка</label>
-                        <input type="file" id="picture" name="picture" />
+                        <label htmlFor="uploadPicture">Добави снимка</label>
+                        <input type="file" id="picture" name="uploadPicture" /* onChange={this.onSubmitChangeHandler} */ />
                         {/* <input type="submit" value="Submit"> */}
                     </fieldset>
                     
                     <div className="middle">
                         <fieldset className="right">
                         <label htmlFor="panneauxName">Име на Паното</label>
-                        <input type="text" id="panneauxName" name="panneauxName" placeholder="text"/>
+                        <input 
+                            type="text" 
+                            id="panneauxName" 
+                            name="panneauxName" 
+                            value={this.state.panneauxName} 
+                            onChange={this.onChangeHandler} 
+                            placeholder="text"
+                        />
                         
-                    </fieldset>
-                    <fieldset className="inline">
-                        <span>Автор</span>                                
-                        <input type="radio" id="vladimir" name="author" value="Vladimir"/>                        
-                        <label htmlFor="vladimir">Владимир</label>                                
-                        <input type="radio" id="damyan" name="author" value="Damyan"/>
-                        <label htmlFor="damyan">Дамян</label>
-                        <input type="radio" id="konstantin" name="author" value="Konstantin"/>
-                        <label htmlFor="konstantin">Константин</label>
-                    </fieldset>
-                    <fieldset>
-                        <label htmlFor="created">Създадена</label>
-                        <input type="date" name="created" id="created"/>
-                    </fieldset>
-                    <fieldset>
-                        <legend>Размери на паното</legend>
-                        <label htmlFor="width">Широчина</label>
-                        <input type="number" name="width" id="width" placeholder="number"/>
-                        <label htmlFor="height">Височина</label>
-                        <input type="number" name="height" id="height" placeholder="number"/>
-                        
-                        <input type="radio" id="cm" name="unit" value="cm"/>
-                        <label htmlFor="cm">cm</label>
-                        
-                        <input type="radio" id="inch" name="unit" value="inch"/>
-                        <label htmlFor="inch">inch</label>
-                    </fieldset>
-                    
-                    
+                        </fieldset>
+                        <fieldset className="inline">
+                            <span>Автор</span>                                
+                            <input type="radio" id="vladimir" name="author" value="Vladimir"/>                        
+                            <label htmlFor="vladimir">Владимир</label>                                
+                            <input type="radio" id="damyan" name="author" value="Damyan"/>
+                            <label htmlFor="damyan">Дамян</label>
+                            <input type="radio" id="konstantin" name="author" value="Konstantin"/>
+                            <label htmlFor="konstantin">Константин</label>
+                        </fieldset>
+                        <fieldset>
+                            <label htmlFor="created">Създадена</label>
+                            <input type="date" name="created" id="created"/>
+                        </fieldset>
+                        <fieldset>
+                            <legend>Размери на паното</legend>
+                            <label htmlFor="width">Широчина</label>
+                            <input type="number" name="width" defaultValue="75" id="width" placeholder="number"/>
+                            <label htmlFor="height">Височина</label>
+                            <input type="number" name="height" defaultValue="75" id="height" placeholder="number"/>
+                            
+                            <input type="radio" id="cm" name="unit" value="cm"/>
+                            <label htmlFor="cm">cm</label>
+                            
+                            <input type="radio" id="inch" name="unit" value="inch"/>
+                            <label htmlFor="inch">inch</label>
+                        </fieldset>
                         <fieldset>
                             <legend>Използвани материали:</legend>
                             <input type="checkbox" id="marbel" name="marbel" value="Marbel"/>
@@ -131,11 +184,10 @@ class Edit extends Component {
                             <input type="checkbox" id="agate" name="agate" value="Аgate"/>
                             <label htmlFor="agate">Ахат</label>
                             <br/>
-                            
                         </fieldset>
                     </div>
                     
-                    <fieldset class=" right w-1 h-3 flexColumn ">
+                    <fieldset className=" right w-1 h-3 flexColumn ">
                         <div className="flexRow">
                             <input type="reset" name="reset" value="Изчисти"/>
                             <label htmlFor="reset">
@@ -164,7 +216,7 @@ class Edit extends Component {
                     
                 </form>
                 
-                <button onClick={logout} className="logout">
+                <button /* onClick={logout} */ className="logout">
                     Logout
                 </button>
                 
