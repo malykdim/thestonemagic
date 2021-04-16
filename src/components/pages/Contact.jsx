fac1a80636396b0b1d769d0b88567c88c903613a
Albena Kostova <albena.kostov@gmail.com>
2021-04-16 21:21:05 +0200

    Removing googleMaps from Contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 // import { Route} from 'react-router-dom' ;
-import Map from '../parts/Map';
 import ContactForm from '../forms/ContactForm';
 import '../main.scss';
 import './Contact.scss';
@@ -16,23 +15,11 @@ class Contact extends Component {
     // }
 
     render() {
-        
-        
-        const location = {
-            address: 'ul. "Hristo G. Danov" 11, 4000 Tsentar, Plovdiv, Bulgaria',
-            lat: 42.148215594449134,
-            lng: 24.74586854733439
-        }
-        
         return (
             <main className="AppMain">
                 <h2>Contact Page</h2>
                 
                 <section className="content">
-                    <div className="map">
-                        <Map location={location} zoomLevel={17}/>                        
-                    </div>
-                    
                     <ContactForm />
                 </section>
             </main>
